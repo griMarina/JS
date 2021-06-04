6f43650b7859a5e868e45fb292f4ed5935f94253
marina <mgrigoreva12@gmail.com>
2021-06-04 17:17:37 +0300

    1 done!

--- hw-3.js
+++ hw-3.js
@@ -1,45 +1,40 @@
 // задание № 1
 
 let i = 2;
-let j = 2;
-while (i <= 100) {
-    while (j < i) {
-        if (i % j == 0) {
-            j++;
+checki: while (i <= 100) {
+    checkj: for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
             i++;
-            continue;
+            continue checki;
         }
     }
     console.log(i++);
 }
 
-
-
 // задание № 2
 
-// function countBasketPrice(array) {
-//     let sum = 0;
-//     for (let item = 0; item < array.length; item++) {
-//         sum += array[item]
-//     }
-//     return sum;
-// }
+function countBasketPrice(array) {
+    let sum = 0;
+    for (let item = 0; item < array.length; item++) {
+        sum += array[item]
+    }
+    return sum;
+}
 
-// let itemsInBasket = [100, 35, 40, 25];  // массив с товарами в корзине
-// let sumPrice = countBasketPrice(itemsInBasket); // общая стоимость всех товаров в корзине
-// console.log(sumPrice);
+let itemsInBasket = [100, 35, 40, 25];  // массив с товарами в корзине
+console.log('Oбщая стоимость всех товаров в корзине: ' + countBasketPrice(itemsInBasket) + '$');
 
 // // задание № 3
 
-// for (let i = 0; i <= 9; console.log(i++)) {
+for (let i = 0; i <= 9; console.log(i++)) {
 
-// };
+};
 
 // // задание № 4
 
-// let x = 'x';
+let x = 'x';
 
-// for (let i = 0; i <= 20; i++) {
-//     console.log(x);
-//     x += 'x';
-// };
+for (let i = 0; i <= 20; i++) {
+    console.log(x);
+    x += 'x';
+};
