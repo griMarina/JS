7433cc2cc72361a2de776c576428562f2fe7f34b
marina <mgrigoreva12@gmail.com>
2021-06-08 13:40:34 +0300

    ex 2

--- hw-4.js
+++ hw-4.js
@@ -42,6 +42,24 @@ console.log(convertNumToObj2(num));
 
 // Задание № 2
 
-let Basket = [
+function item(name, price, size, color, quantity) {
+    this.name = name,
+        this.price = price,
+        this.size = size,
+        this.color = color,
+        this.quantity = quantity;
+};
 
-]
+let item1 = new item('dress', 100, 's', 'black', 1);
+let item2 = new item('shirt', 80, 'm', 'green', 1);
+let item3 = new item('bag', 200, 'oneSize', 'black', 1);
+let item4 = new item('socks', 20, 's', 'black', 2);
+
+let basket = [item1, item2, item3, item4];
+console.log(basket);
+
+let totalSum = basket.reduce((sum, item) => {
+    sum += (item.price * item.quantity);
+    return sum;
+}, 0);
+console.log(totalSum);
