d2b36d04ee9a3c0a801accb14f4f8009ee3373cc
marina <mgrigoreva12@gmail.com>
2021-06-12 23:06:26 +0300

    cart2

--- cart.js
+++ cart.js
@@ -1,26 +1,43 @@
 'use strict'
 
+let cart = document.querySelector('.cart');
+
 function item(name, price, size, color, quantity) {
     this.name = name,
         this.price = price,
         this.size = size,
         this.color = color,
         this.quantity = quantity;
-};
+}
 
-let item1 = new item('dress', 100, 's', 'black', 1);
-let item2 = new item('shirt', 80, 'm', 'green', 1);
-let item3 = new item('bag', 200, 'oneSize', 'black', 1);
-let item4 = new item('socks', 20, 's', 'black', 2);
+function countQuantity(array) {
+    let sum = 0;
+    for (let item of array) {
+        sum += item.quantity;
+    }
+    return sum;
+}
 
-let basket = [item1, item2, item3, item4];
-console.log(basket);
-
-function countBasketPrice(array) {
+function countCartPrice(array) {
     let sum = 0;
     for (let item of array) {
         sum += (item.price * item.quantity);
     }
     return sum;
 }
-console.log(countBasketPrice(basket));
+
+let cartArr = [
+    new item('dress', 1000, 's', 'black', 1),
+    new item('shirt', 800, 'm', 'green', 2),
+    new item('bag', 2000, 'oneSize', 'black', 1),
+    new item('socks', 200, 's', 'black', 3),
+];
+
+let quantityTotal = countQuantity(cartArr);
+let sumTotal = countCartPrice(cartArr);
+
+if (quantityTotal === 0) {
+    cart.textContent = 'Корзина пуста'
+} else {
+    cart.textContent = `В корзине ${quantityTotal} товаров на сумму ${sumTotal}`
+};
