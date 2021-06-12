89b7904b5b031296a3d979ba65a0f04c035b051c
marina <mgrigoreva12@gmail.com>
2021-06-12 21:00:52 +0300

    chess board

--- chess.js
+++ chess.js
@@ -1,50 +1,35 @@
-const board = document.querySelector('.board');
-
-function createLetRow() {
-    let div = document.createElement('div');
-    let row = board.appendChild(div)
-    row.classList.add('row');
-    let col;
-    let n = 0;
-    const letList = 'ABCDEFGH'.split('');
-    for (let i = 0; i < 9; i++) {
-        div = document.createElement('div');
-        col = row.appendChild(div);
-        col.classList.add('col');
-        if (i < 1) {
-            col.classList.add('col-num');
-        } else {
-            col.classList.add('row-let');
-            col.textContent = letList[n++];
-        }
-    }
-}
-
 function createBoard() {
-    createLetRow();
-    for (let j = 1; j < 9; j++) {
+    const board = document.querySelector('.board');
+    let n = 0; // нумерация строк
+    const letList = 'ABCDEFGH'.split('');
+    for (let j = 0; j < 9; j++) { // цикл создания строк с классом row
         let div = document.createElement('div');
         let row = board.appendChild(div);
         row.classList.add('row');
-        let col;
-        let colNext;
-        let n = 0;
-        i: for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < 9; i++) { // цикл создания столбцов с классом  col
             div = document.createElement('div');
-            col = row.appendChild(div);
+            let col = row.appendChild(div);
             col.classList.add('col');
-            colNext = col.previousElementSibling;
-            if (i < 1) {
+            let colPrev = col.previousElementSibling; // предыдущая ячейка
+            // заполняем ячейки 
+            if (i === 0 && j === 0) {
+                col.classList.add('col-num'); // нулевая пустая ячейка с классом col-num
+            }
+            else if (i >= 1 && j === 0) { // нулевая строка с нумерацией буквами c классом row-let
+                col.classList.add('row-let');
+                col.textContent = letList[n++];
+            }
+            else if (i === 0 && j > 0) { // нулевой стоблец с нумерацией числами с классом col-num
                 col.classList.add('col-num');
                 col.textContent = j;
-            } else if (colNext && j % 2 == 0) {
-                colNext.classList.contains('black') ? col.classList.add('white') : col.classList.add('black');
             }
-            else if (i > 1 && colNext && j % 2 !== 0) {
-                colNext.classList.contains('black') ? col.classList.add('white') : col.classList.add('black');
+            // в строках с четным номером, если у предыдущей ячейки нет класса black, добавляем класс black
+            else if (colPrev && (j % 2) === 0) {
+                colPrev.classList.contains('black') ? 0 : col.classList.add('black');
             }
-            else {
-                continue i;
+            // начиная со второго столбца в строках с нечетным номером, если у предыдущей ячейки нет класса black, добавляем класс black
+            else if (i > 1 && colPrev && (j % 2) !== 0) {
+                colPrev.classList.contains('black') ? 0 : col.classList.add('black');
             }
         }
     }
