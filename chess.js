7dd14b1bca5c564de0e100c9fa54c216b4afdc04
marina <mgrigoreva12@gmail.com>
2021-06-13 11:07:35 +0300

    chess correction

--- chess.js
+++ chess.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function createBoard() {
+function createChessBoard() {
     const board = document.querySelector('.board');
     const letList = 'ABCDEFGH'.split('');
     for (let j = 0; j < 10; j++) { // цикл создания строк с классом row
@@ -12,29 +12,26 @@ function createBoard() {
             div = document.createElement('div');
             let col = row.appendChild(div);
             col.classList.add('col');
-            let colPrev = col.previousElementSibling; // предыдущая ячейка
             // заполняем ячейки 
             if ((i === 0 && j === 0) || (i === 0 && j === 9)) {
-                col.classList.add('col-num'); // нулевые пустые ячейка с классом col-num
+                col.classList.add('col-num'); // пустые ячейки с классом col-num
             }
             else if ((i >= 1 && j === 0) || (i >= 1 && j === 9)) { // нулевая и последняя строки с нумерацией буквами c классом row-let
                 col.classList.add('row-let');
                 col.textContent = letList[n++];
             }
-            else if ((i === 0 && 0 < j < 9) || (i === 9 && 0 < j < 9)) { // нулевой и последний столбцы с нумерацией числами с классом col-num
+            else if ((i === 0 && j < 9) || (i === 9 && j < 9)) { // нулевой и последний столбцы с нумерацией числами с классом col-num
                 col.classList.add('col-num');
                 col.textContent = j;
             }
-            // в строках с четным номером, если у предыдущей ячейки нет класса black, добавляем класс black  
-            else if (colPrev && (j % 2) === 0) {
-                colPrev.classList.contains('black') ? 0 : col.classList.add('black');
+            else if ((i % 2) !== 0 && (j % 2) === 0) { // в строках с четным номером, у стоблцов с нечетным номером добавляем класс black  
+                col.classList.add('black');
             }
-            // начиная со второго столбца в строках с нечетным номером, если у предыдущей ячейки нет класса black, добавляем класс black
-            else if (i > 1 && colPrev && (j % 2) !== 0) {
-                colPrev.classList.contains('black') ? 0 : col.classList.add('black');
+            else if ((i % 2) === 0 && (j % 2) !== 0) { // в строках с нечетным номером, у стоблцов с четным номером добавляем класс black
+                col.classList.add('black');
             }
         }
     }
 }
 
-createBoard();
+createChessBoard();
