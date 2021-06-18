f4ab1b32dd1784f8b438a2d7958b9830480c6022
marina <mgrigoreva12@gmail.com>
2021-06-18 22:47:49 +0300

    ex 1

--- shop.js
+++ shop.js
@@ -6,30 +6,19 @@ const $catalog = document.querySelector('.catalog');
 let cart = [];
 let catalog = [];
 
-function getID() {
-    let count = 1;
-    return function () {
-        return count++;
-    }
+function Item(title, img, desc, price, id) {
+    this.title = title;
+    this.img = img;
+    this.desc = desc;
+    this.price = price;
+    this.id = id;
 }
 
-let idItem = getID();
-let idItemCart = getID();
-
-
-function Item(title, img, desc, price) {
-    this.title = title,
-        this.img = img,
-        this.desc = desc,
-        this.price = price,
-        this.id = idItem();
-}
-
-function ItemCart(title, price, quantity) {
-    this.title = title,
-        this.price = price,
-        this.quantity = quantity,
-        this.id = idItemCart();
+function ItemCart({ title, price, quantity = 1, id }) {
+    this.title = title;
+    this.price = price;
+    this.quantity = quantity;
+    this.id = id;
 }
 
 function countQuantity(array) { // подсчет количества товаров в корзине
@@ -55,52 +44,41 @@ function createCatalog(array) { // создание каталога с карт
 }
 
 function createItemCard({ title, img, desc, price, id }) { // создание карточки товара
-    const itemHtml = `<div class="card">
-    <h2 class="card__title">${title}</h2>
-    <img src="${img}" alt="image" class="card__img">
-    <p class="card__desc">${desc}</p>
-    <p class="card__price">${price} руб.</p>
-    <button data-id="${id}" class="card__btn">Add to Cart</button>
+    const itemHtml = `<div class="item-card ">
+    <h2 class="item-card__title ">${title}</h2>
+    <img src="${img}" alt="image" class="item-card__img ">
+    <p class="item-card__desc">${desc}</p>
+    <p class="item-card__price">${price} руб.</p>
+    <button data-id="${id}" class="item-card__btn">Add to Cart</button>
     </div>`;
     $catalog.insertAdjacentHTML('beforeend', itemHtml);
 }
 
-// function createCatalog(array) { // создание каталога с карточками
-//     for (let item of array) {
-
-//     }
-// }
-
-const cartArr = [
-    new ItemCart('Rucksack', 1000, 1),
-    new ItemCart('Suit', 3000, 1),
-    new ItemCart('Jacket', 4000, 1),
-    new ItemCart('Trousers', 2000, 1),
-    new ItemCart('Jacket', 1500, 1),
-    new ItemCart('Shirt', 1200, 1)
-];
-console.log(cartArr);
+function createCartText() { // текст корзины
+    if (countQuantity(cart) > 0) {
+        $cart.textContent = `В корзине ${countQuantity(cart)} товаров на сумму ${countCartPrice(cart)} рублей`;
+    } else {
+        $cart.textContent = 'Корзина пуста';
+    };
+}
 
 catalog = [
-    new Item('Rucksack', 'img/item-1.jpg', 'Item description', 1000),
-    new Item('Suit', 'img/item-2.jpg', 'Item description', 3000),
-    new Item('Jacket', 'img/item-3.jpg', 'Item description', 4000),
-    new Item('Trousers', 'img/item-4.jpg', 'Item description', 2000),
-    new Item('Jacket', 'img/item-5.jpg', 'Item description', 1500),
-    new Item('Shirt', 'img/item-6.jpg', 'Item description', 1200)
+    new Item('Rucksack', 'img/item-1.jpg', 'Item description', 1000, 0),
+    new Item('Suit', 'img/item-2.jpg', 'Item description', 3000, 1),
+    new Item('Jacket', 'img/item-3.jpg', 'Item description', 4000, 2),
+    new Item('Trousers', 'img/item-4.jpg', 'Item description', 2000, 3),
+    new Item('Jacket', 'img/item-5.jpg', 'Item description', 1500, 4),
+    new Item('Shirt', 'img/item-6.jpg', 'Item description', 1200, 5)
 ];
-console.log(catalog);
 
-// Вывод текста в корзине
-if (countQuantity(cart) === 0) {
-    $cart.textContent = 'Корзина пуста';
-} else {
-    $cart.textContent = `В корзине ${countQuantity(cart)} товаров на сумму ${countCartPrice(cart)} рублей`;
-};
-
-// вывод товаров из массива каталог
 createCatalog(catalog);
+createCartText();
 
-// $catalog.addEventListener('click', function () {
-//     $cart.appendChild('.card')
-// })
+// добавление товара в корзину
+$catalog.addEventListener('click', function (event) {
+    if (event.target.className === 'item-card__btn') {
+        let dataId = Number(event.target.getAttribute('data-id'));
+        cart.push(new ItemCart(catalog[dataId]));
+    }
+    createCartText();
+})
